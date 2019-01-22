711576cef86220b4db7182bb2646f81e87695469
Cane <canela@bullestate.cl>
2019-01-22 15:22:54 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'pedidos#index'
   resources :pedidos
 
-  get 'pedidos/listado' => 'pedido#listado'
+  get 'pedidos/listado' => 'pedidos#listado'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
