03fff7f6d0511bfa979059d58bee70a6f2a291e0
Cane <canela@bullestate.cl>
2019-01-22 17:19:37 -0300

    lectura js

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -20,4 +20,4 @@ Rails.application.config.assets.precompile += %w( index.js )
 Rails.application.config.assets.precompile += %w( helper.css )
 Rails.application.config.assets.precompile += %w( fonts-icons.css )
 Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf|otf|woff2)\z/
-Rails.application.config.assets.precompile += %w( TipoBaño.js )
+Rails.application.config.assets.precompile += %w( tipobano.js )
