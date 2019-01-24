a3fdf7e19c3d92b3abf6d8a5a84334a8ad5c481d
Cane <canela@bullestate.cl>
2019-01-24 13:20:24 -0300

    Update pedido.rb

--- app/models/pedido.rb
+++ app/models/pedido.rb
@@ -3,10 +3,10 @@ class Pedido < ApplicationRecord
         :presence => true,
         :format => { :with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/ ,
                      :message => 'Correo inválido! Porfavor ingresar correo correctamente'}
-    validates :nombre, :presence => true, :format =>  {:message => 'Campo requerido'}
-    validates :telefono, :presence => true, :format =>  {:message => 'Campo requerido'}
-    validates :tipo, :presence => true, :format =>  {:message => 'Campo requerido'}
-    validates :operacion, :presence => true, :format =>  {:message => 'Campo requerido'}
-    validates :direccion, :presence => true, :format =>  {:message => 'Campo requerido'}
-    
+    validates :nombre, :presence => true, :message => 'Campo requerido'
+    validates :telefono, :presence => true, :message => 'Campo requerido'
+    validates :tipo, :presence => true, :message => 'Campo requerido'
+    validates :operacion, :presence => true, :message => 'Campo requerido'
+    validates :direccion, :presence => true, :message => 'Campo requerido'
+
 end
