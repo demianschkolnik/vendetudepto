3c3c0c33da36d7851d66c9b5ddfef5aa38ffb61b
Cane <canela@bullestate.cl>
2019-01-28 10:21:57 -0300

    boton en exito

--- app/models/pedido.rb
+++ app/models/pedido.rb
@@ -1,9 +1,10 @@
 class Pedido < ApplicationRecord
+    
     validates :mail,   
         :presence => true, presence: { message: "Este campo es obligatorio." },
         :format => { :with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/ ,
                      :message => 'Correo inválido! Porfavor ingresar correo correctamente'},
-        uniqueness: {message: "already subscribed"}
+        uniqueness: {message: "Ya ingresado."}
         
     validates :nombre, :presence => true, presence: { message: "Este campo es obligatorio." }
     validates :telefono, :presence => true, presence: { message: "Este campo es obligatorio."  },
