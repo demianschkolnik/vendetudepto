e249f32da4edcd221ebfabfb70c00ae3b93768df
Cane <canela@bullestate.cl>
2019-01-28 10:43:23 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'pedidos#index'
  
-  get 'pedidos', to: 'pedidos#show'
+  post 'pedidos', to: 'pedidos#show'
   resources :pedidos do
     collection { post :validate }
   end
