3f6967daa046b63f8d18475c9046449809e9fdf5
Cane <canela@bullestate.cl>
2019-01-31 11:19:43 -0300

    prueba lat/lng

--- app/assets/javascripts/direccion.js
+++ app/assets/javascripts/direccion.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
     // Call Geo Complete
-    $("#address").geocomplete({details:"form#property"});
+    $("#address").geocomplete({details:"form#new_pedido"});
   });
   
