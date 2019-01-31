0e61b893cf24ebbbab06224fa84f12f118b6a72f
Cane <canela@bullestate.cl>
2019-01-31 15:03:52 -0300

    Revert "Update pedidos_controller.rb"
    This reverts commit 18703b7c8f6b9e1db3cd6440f67b5ed761971d76.

--- app/controllers/pedidos_controller.rb
+++ app/controllers/pedidos_controller.rb
@@ -80,7 +80,7 @@ class PedidosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pedido_params
-      params.permit(:nombre, 
+      params.require(:pedido).permit(:nombre, 
       :mail, 
       :telefono, 
       :descripcion, 
